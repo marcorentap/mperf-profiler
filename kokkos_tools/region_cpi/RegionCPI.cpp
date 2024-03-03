e42222cc12dc30ba8dd12ed5715f73f2b3f77a97
Marc Owen <marcowen20@gmail.com>
2024-03-03 20:28:07 +0900

    Calculate and output average CPI and IPC

--- kokkos_tools/region_cpi/RegionCPI.cpp
+++ kokkos_tools/region_cpi/RegionCPI.cpp
@@ -15,6 +15,7 @@ using PerfTracer = MPerf::Tracers::LinuxPerf::Tracer;
 constexpr auto outputFileName = "mperf_region_cpi.json";
 
 std::stack<std::string> regionNameStack;
+std::unordered_map<std::string, std::vector<double>> regionCPIs;
 std::ofstream outputFile;
 json eventJson, cpiJson;
 std::unique_ptr<Measure> cpuEventMeasure;
@@ -23,14 +24,57 @@ extern "C" void kokkosp_init_library(const int loadSeq,
                                      const uint64_t interfaceVer,
                                      const uint32_t devInfoCount,
                                      void *deviceInfo) {
-  // Measure processor events with linux's perf 
+  // Measure processor events with linux's perf
   auto perfTracer = PerfTracer();
   cpuEventMeasure = perfTracer.MakeMeasure(MPerf::HLMeasureType::ProcCounters);
   outputFile.open(outputFileName, std::ofstream::trunc);
 }
 
 extern "C" void kokkosp_finalize_library() {
-  outputFile << eventJson << std::endl;
+  auto eventStack = std::stack<json>{};
+
+  // Calculate CPIs per region
+  for (auto &event : eventJson) {
+    if (event["hook"] == "kokkosp_push_profile_region") {
+      eventStack.push(event);
+      continue;
+    }
+
+    if (event["hook"] == "kokkosp_pop_profile_region") {
+      json regionCPI;
+      json regionEnd = event;
+      json regionBegin = eventStack.top();
+      std::string regionName = regionBegin["region_name"];
+
+      uint64_t beginInsts = regionBegin["instructions"];
+      uint64_t endInsts = regionEnd["instructions"];
+      uint64_t beginCycles = regionBegin["cycles"];
+      uint64_t endCycles = regionEnd["cycles"];
+      uint64_t instDiff = endInsts - beginInsts;
+      uint64_t cycleDiff = endCycles - beginCycles;
+      double CPI = (double)cycleDiff / instDiff;
+
+      regionCPIs[regionName].push_back(CPI);
+      eventStack.pop();
+    }
+  }
+
+  // Calculate average CPI
+  json outJson;
+  for (auto &region : regionCPIs) {
+    json regionJson;
+    auto CPIs = region.second;
+    auto regionName = region.first;
+    double averageCPI =
+        (double)std::accumulate(CPIs.begin(), CPIs.end(), 0.0) / CPIs.size();
+
+    regionJson["region_name"] = region.first;
+    regionJson["cpi"] = averageCPI;
+    regionJson["ipc"] = 1.0 / averageCPI;
+    outJson.push_back(regionJson);
+  }
+
+  outputFile << outJson << std::endl;
 }
 
 extern "C" void kokkosp_push_profile_region(char *regionName) {
@@ -40,7 +84,7 @@ extern "C" void kokkosp_push_profile_region(char *regionName) {
   cpuEvents = cpuEventMeasure->GetJSON();
 
   regionNameStack.push(regionName);
-  out["regionName"] = regionName;
+  out["region_name"] = regionName;
   out["instructions"] = cpuEvents["insts"];
   out["cycles"] = cpuEvents["cycles"];
   out["hook"] = __FUNCTION__;
@@ -55,7 +99,7 @@ extern "C" void kokkosp_pop_profile_region() {
   cpuEvents = cpuEventMeasure->GetJSON();
   regionName = regionNameStack.top();
 
-  out["regionName"] = regionName;
+  out["region_name"] = regionName;
   out["instructions"] = cpuEvents["insts"];
   out["cycles"] = cpuEvents["cycles"];
   out["hook"] = __FUNCTION__;
